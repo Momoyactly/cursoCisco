1cdc1aa0e1aa40c7f38b2c1d60a7d8135b500aed
Uriel Arriaga <joarriag@cisco.com>
2020-01-12 13:54:55 -0600

    primer netconf script

---
+++ netconf/netconf1.py
@@ -0,0 +1,9 @@
+from ncclient import manager
+
+with manager.connect(host="172.16.1.11",
+                    port="830",
+                    username="uriel",
+                    password="cisco123", 
+                    hostkey_verify=False) as m:
+    for capacidad in m.server_capabilities:
+        print(capacidad)
