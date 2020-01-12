da48aa8c5405bf5a90786681e7900c17210359fa
Uriel Arriaga <joarriag@cisco.com>
2020-01-12 14:53:54 -0600

    4to netconf script

---
+++ netconf/netconf4.py
@@ -0,0 +1,20 @@
+from ncclient import manager
+import xml.dom.minidom
+import xmltodict
+from router_info import router
+
+netconf_filter = open("/Users/joarriag/Dev/cursoCisco/netconf/netconf_Filter.xml").read()
+
+with manager.connect(host=router["host"],
+                    port=router["port"],
+                    username=router["username"],
+                    password=router["password"], 
+                    hostkey_verify=False) as m:
+    interface_netconf = m.get(netconf_filter)
+    interfaceDict = xmltodict.parse(interface_netconf.xml)["rpc-reply"]["data"]
+    nombre = interfaceDict["interfaces"]["interface"]["name"]["#text"]
+    config = interfaceDict["interfaces"]
+    op_status = interfaceDict["interfaces-state"]
+    print(f"nombre: {nombre}")
+    print("Habilitada:",config["interface"]["enabled"])
+    print("Paquetes entrando:",op_status["interface"]["statistics"]["in-unicast-pkts"])
---
+++ netconf/router_info.py
@@ -0,0 +1,4 @@
+router ={"host":"172.16.1.11",
+        "port":"830",
+        "username":"uriel",
+        "password":"cisco123" }
