6623dafa14d5009785203c27fb0eda21a4383944
Uriel Arriaga <joarriag@cisco.com>
2020-01-12 15:27:21 -0600

    config netconf script v2

--- netconf/netconf_config.py
+++ netconf/netconf_config.py
@@ -1,5 +1,6 @@
 from ncclient import manager
 from router_info import router
+import xmltodict
 
 configInterfaceTemplate = open("/Users/joarriag/Dev/cursoCisco/netconf/ios_config.xml").read()
 netconfConfig = configInterfaceTemplate.format(interfaceName="GigabitEthernet2",interfaceDesc="Uriel wuz here")
@@ -10,4 +11,8 @@ with manager.connect(host=router["host"],
                     password=router["password"], 
                     hostkey_verify=False) as m:
     deviceReply = m.edit_config(netconfConfig, target="running")
-    print(deviceReply)
+    replyDict = xmltodict.parse(deviceReply.xml)["rpc-reply"]
+    if 'ok' in replyDict:
+        print("OK")
+    else:
+        print("Fail")
