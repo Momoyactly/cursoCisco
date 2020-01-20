54ed439d86fe5f63be6ea7280d726e275b1b9f7f
Uriel Arriaga <joarriag@cisco.com>
2020-01-19 18:26:45 -0600

    Network Automation V1

--- networkAutomation/netconf.py
+++ networkAutomation/netconf.py
@@ -4,6 +4,7 @@ import xmltodict
 from router_info import router
 
 netconf_filter = open("/Users/joarriag/Dev/cursoCisco/networkAutomation/netconfCdpFilter.xml").read()
+configInterfaceTemplate = open("/Users/joarriag/Dev/cursoCisco/networkAutomation/iosXeConfig.xml").read()
 
 with manager.connect(host=router["host"],
                     port=router["port"],
@@ -11,13 +12,15 @@ with manager.connect(host=router["host"],
                     password=router["password"], 
                     hostkey_verify=False) as m:
     interface_netconf = m.get(netconf_filter)
-    interfaceDict = xmltodict.parse(interface_netconf.xml)
-    print(interfaceDict["rpc-reply"]["data"])
-    """
-    nombre = interfaceDict["interfaces"]["interface"]["name"]["#text"]
-    config = interfaceDict["interfaces"]
-    op_status = interfaceDict["interfaces-state"]
-    print(f"nombre: {nombre}")
-    print("Habilitada:",config["interface"]["enabled"])
-    print("Paquetes entrando:",op_status["interface"]["statistics"]["in-unicast-pkts"])
-    """
+    interfaceDict = xmltodict.parse(interface_netconf.xml)["rpc-reply"]["data"]
+    cdpNeighName = interfaceDict["cdp-neighbor-details"]["cdp-neighbor-detail"]["device-name"]
+    localIntName = interfaceDict["cdp-neighbor-details"]["cdp-neighbor-detail"]["local-intf-name"]
+    print("vecino:",cdpNeighName,"interfaz:",localIntName)
+
+    netconfConfig = configInterfaceTemplate.format(interfaceName=localIntName,interfaceDesc=cdpNeighName+" es el vecino en la interfaz: "+localIntName)
+    deviceReply = m.edit_config(netconfConfig, target="running")
+    replyDict = xmltodict.parse(deviceReply.xml)["rpc-reply"]
+    if 'ok' in replyDict:
+        print("OK")
+    else:
+        print("Fail")
